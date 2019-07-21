116ac6c7aff3224dde239c36cc876f7042956e34
Hardian Prakasa <hardian.prakasa@dana.id>
2019-07-21 22:33:12 +0700

    Wrap MapView for SwiftUI

---
+++ Nearby Me/MapView.swift
@@ -0,0 +1,34 @@
+//
+//  MapView.swift
+//  Nearby Me
+//
+//  Created by Hardian Prakasa on 21/07/19.
+//  Copyright © 2019 DANA Indonesia. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: -6.232008, longitude: 106.820384)
+        let span = MKCoordinateSpan(latitudeDelta: 0.003, longitudeDelta: 0.003)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        view.setRegion(region, animated: true)
+    }
+
+}
+
+#if DEBUG
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
+#endif
