192e3bad515354668b355dee511087c4cce7d041
Hardian Prakasa <hardian.prakasa@dana.id>
2019-07-23 06:47:38 +0700

    Use a bindable object for storage

--- Nearby Me/Models/MerchantsFactory.swift
+++ Nearby Me/Models/MerchantsFactory.swift
@@ -8,7 +8,6 @@
 
 import Foundation
 
-
 let merchants: [Merchant] = MerchantsFactory().getMerchants()
 
 struct MerchantsFactory {
---
+++ Nearby Me/Models/UserData.swift
@@ -0,0 +1,27 @@
+//
+//  UserData.swift
+//  Nearby Me
+//
+//  Created by Hardian Prakasa on 23/07/19.
+//  Copyright © 2019 DANA Indonesia. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: BindableObject {
+    
+    let willChange = PassthroughSubject<Void, Never>()
+    
+    var showFavoritesOnly = false {
+        didSet {
+            willChange.send()
+        }
+    }
+    
+    var merchantsData = merchants {
+        didSet {
+            willChange.send()
+        }
+    }
+}
